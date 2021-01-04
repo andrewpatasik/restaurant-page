ce6b517b8c45a44366d1b18db8c650dbb17d671e
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-01-04 10:23:12 +0800

    tweaked some elements

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// console.log('worked');
+console.log('worked');
 
 let dropdown = document.querySelector('.dropdown');
 let dropdownItem = document.querySelector('.dropdown-item');
 
 dropdown.addEventListener('click', (e) => {
     dropdownItem.classList.toggle('show');
+    console.log('clicked');
 })
