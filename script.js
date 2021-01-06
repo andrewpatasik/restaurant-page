88934976aadcf7f51cd540a684c37a984b526b2e
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-01-07 06:07:24 +0800

    responsive navbar, drop down menu using icon, showcase container position adjusted

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ console.log('worked');
 
 let dropdown = document.querySelector('.dropdown');
 let dropdownItem = document.querySelector('.dropdown-item');
+let icon = document.getElementById('nav-icon');
 let arrowL = document.getElementById('arrow-left');
 
 dropdown.addEventListener('click', (e) => {
+    dropdownItem.classList.toggle('show');
+    console.log('clicked');
+})
+
+icon.addEventListener('click', (e) => {
     dropdownItem.classList.toggle('show');
     console.log('clicked');
 })
