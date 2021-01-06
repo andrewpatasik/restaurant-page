3e05d7fbd05e1f55d82f79e51f0daf8c5d1cb304
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-01-06 09:17:29 +0800

    refactor code, desktop version, mobile version

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ console.log('worked');
 
 let dropdown = document.querySelector('.dropdown');
 let dropdownItem = document.querySelector('.dropdown-item');
+let arrowL = document.getElementById('arrow-left');
 
 dropdown.addEventListener('click', (e) => {
     dropdownItem.classList.toggle('show');
     console.log('clicked');
+})
+
+arrowL.addEventListener('click', (e) => {
+  
 })
