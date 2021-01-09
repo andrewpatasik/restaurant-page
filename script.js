8bb9c004a92512158689047bd748b839dcc973b3
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-01-09 13:40:27 +0800

    JS: image slider, click arrow to change slider

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let icon = document.getElementById('nav-icon');
 let arrowL = document.getElementById('arrow-left');
 let arrowR = document.getElementById('arrow-right');
 
-
-// 1. simpan assets di dalam array
 let imageList = [
     { 
         id: 'image-1',
@@ -23,12 +21,10 @@ let imageList = [
 
 dropdown.addEventListener('click', (e) => {
     dropdownItem.classList.toggle('show');
-    // console.log('clicked');
 })
 
 icon.addEventListener('click', (e) => {
     dropdownItem.classList.toggle('show');
-    // console.log('clicked');
 })
 
 // close dropdown item when clicking everywhere but navigatio/icon
@@ -39,41 +35,13 @@ window.addEventListener('click', (e) => {
 })
 
 arrowL.addEventListener('click', (e) => {
-//   if (e.target === './images/edited/slider-1.png') {
-//       console.log(e.target);
-//     //   createImageDOM(imageList[2].src);
-//   } else if (e.target === './images/edited/slider-2.png') {
-//     // createImageDOM(imageList[1].src);
-//     console.log(e.target);
-//   } else if (e.target === './images/edited/slider-3.png') {
-//     // createImageDOM(imageList[2].src);
-//     console.log(e.target);
-// }
-    displayImage();
-    // console.log(imageContent.id);
+    displayImage('l');
 })
 
 arrowR.addEventListener('click', (e) => {
-    //   if (e.target === './images/edited/slider-1.png') {
-    //       console.log(e.target);
-    //     //   createImageDOM(imageList[2].src);
-    //   } else if (e.target === './images/edited/slider-2.png') {
-    //     // createImageDOM(imageList[1].src);
-    //     console.log(e.target);
-    //   } else if (e.target === './images/edited/slider-3.png') {
-    //     // createImageDOM(imageList[2].src);
-    //     console.log(e.target);
-    // }
-        displayImage();
-        // console.log(imageContent.id);
+        displayImage('r');
     })
 
-// buat slider untuk image content
-
-
-// 2. buat function yang mengeksekusi loop untuk
-// gambar secara berurut
-// setiap satu detik sekali, console log DOM
 function createImageDOM(objId, objSource){
     let img = document.createElement('img');
     img.setAttribute('id', objId)
@@ -88,7 +56,7 @@ function clearDOM(domElement){
     domElement.remove();
 }
 
-function displayImage(){
+function displayImage(arrowDirection){
     let imageContent = document.querySelector('.image-content');
     function insertAfter(referenceNode, newNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
@@ -96,23 +64,37 @@ function displayImage(){
 
     let index = imageContent.id.toString();
 
-        if(index === 'image-1'){
-            clearDOM(imageContent);
-            insertAfter(arrowL, createImageDOM(imageList[1].id, imageList[1].src));
-            console.log(index);
-        } else if(index === 'image-2'){
-            clearDOM(imageContent);
-            insertAfter(arrowL, createImageDOM(imageList[2].id, imageList[2].src));
-            console.log(index);
-        } else if(index === 'image-3'){
-            clearDOM(imageContent);
-            insertAfter(arrowL, createImageDOM(imageList[0].id, imageList[0].src));
-            console.log(index);
-        }
-
+    switch (arrowDirection) {
+        case 'l':
+            if(index === 'image-1'){
+                clearDOM(imageContent);
+                insertAfter(arrowL, createImageDOM(imageList[2].id, imageList[2].src));
+            } else if(index === 'image-2'){
+                clearDOM(imageContent);
+                insertAfter(arrowL, createImageDOM(imageList[0].id, imageList[0].src));
+            } else if(index === 'image-3'){
+                clearDOM(imageContent);
+                insertAfter(arrowL, createImageDOM(imageList[1].id, imageList[1].src));
+            }                
+            break;
+    
+            case 'r':
+                if(index === 'image-1'){
+                    clearDOM(imageContent);
+                    insertAfter(arrowL, createImageDOM(imageList[1].id, imageList[1].src));
+                } else if(index === 'image-2'){
+                    clearDOM(imageContent);
+                    insertAfter(arrowL, createImageDOM(imageList[2].id, imageList[2].src));
+                } else if(index === 'image-3'){
+                    clearDOM(imageContent);
+                    insertAfter(arrowL, createImageDOM(imageList[0].id, imageList[0].src));
+                }                
+                break;
+    }
 }
 
-//  2.1. DOM dibuat secara dinamis setiap slider (optional?landllpahwe) 
-// 3. setInterval untuk function
-// let slide = displayImage();
-// setInterval(slide, 3000);
+function slideImage(){
+    displayImage('r');
+}
+
+setInterval(slideImage, 3000);
